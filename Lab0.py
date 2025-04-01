2f2852c8739d5c0dfab570895fd3df15523a5227
Pilon2020 <123617070+Pilon2020@users.noreply.github.com>
2025-04-01 16:55:48 -0700

    Initial Commit

---
+++ Lab0.py
@@ -0,0 +1,23 @@
+# Oliver Pilon
+# CSC 448
+# 2025-04-01
+
+# Setting up libraries and basics prior to first true lab
+# Install SciPy, NumPy, and sklearn libaries
+
+# You are encouraged to use all built in scientific computing libraries available through SciPy and NumPy. In general, you should never (re-)implement classical computer science algorithms (i.e K-means) 
+# but rather simply call the appropriate library.
+
+# You are also allowed to use scripts and code from GitHub repositories of other scientists of their published research. You should properly cite the corresponding paper, see Project report for more details.
+
+# You are not allowed to use any code from the internet which is not part of a published (in peer reviewed journal) scientific research.
+
+# Library Imports
+import numpy as np      #NumPy Documentation Link: https://numpy.org/doc/2.2/numpy-user.pdf       
+import scipy as sp      #SciPy Documentation Link: https://docs.scipy.org/doc/scipy/tutorial/index.html#user-guide
+import sklearn as sk    #sklearn Documentation Link: https://scikit-learn.org/stable/user_guide.html
+
+print(f'SciPy Version:', sp.__version__)
+print(f'NumPy Version:', np.__version__)
+print(f'sklearn Version:', sk.__version__)
+
